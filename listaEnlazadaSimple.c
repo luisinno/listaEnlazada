69b1fd04c0c9ec76c0c0837f65de8650d1413cb5
luis <lu.cor.ira@gmail.com>
2026-05-19 21:35:01 +0200

    c

--- listaEnlazadaSimple.c
+++ listaEnlazadaSimple.c
@@ -16,6 +16,8 @@
 int
 crearVacia(ListaEnlazadaRef raiz)
 {
+	*raiz=NULL;
+	return 0;
 }
 
 /*
@@ -26,6 +28,7 @@ crearVacia(ListaEnlazadaRef raiz)
 int
 estaVacia(ListaEnlazada raiz)
 {
+	return (raiz==NULL);
 }
 
 /*
@@ -36,6 +39,16 @@ estaVacia(ListaEnlazada raiz)
 tipoNodoRef
 creaNodo(tipoInfoRef info)
 {
+	tipoNodoRef nuevo;
+
+	if(NULL==(nuevo=malloc(sizeof(tipoNodo))))
+	{
+		return NULL;
+	}
+	
+	nuevo->info=*info;
+	nuevo->sig=NULL;
+	return nuevo;
 }
 
 /*
@@ -47,6 +60,16 @@ creaNodo(tipoInfoRef info)
 int
 insertarDespuesDeNodo(ListaEnlazadaRef raiz, tipoNodoRef pos, tipoInfoRef info)
 {
+	tipoNodoRef nuevo;
+	
+	if(NULL==(nuevo=creaNodo(info)) || NULL==pos)
+	return -1;
+
+	if(estaVacia(*raiz))
+	*raiz=nuevo;
+	nuevo->sig=pos->sig;
+	pos->sig=nuevo;
+	return 0;
  }
 
 
@@ -59,6 +82,31 @@ insertarDespuesDeNodo(ListaEnlazadaRef raiz, tipoNodoRef pos, tipoInfoRef info)
 int
 insertarAntesDeNodo(ListaEnlazadaRef raiz, tipoNodoRef pos, tipoInfoRef info)
 {
+	tipoNodoRef nuevo, ant;
+	
+	if(NULL==(nuevo=creaNodo(info)))
+	return -1;
+
+	if(NULL==(*raiz) || NULL==raiz)
+	return -1;
+
+	ant= *raiz; //QUE ANT APUNTE A LA LISTA ENLAZADA
+	if(NULL==pos)
+	return -1;
+	if(pos==*raiz) //INSERTAR ANTES DEL PRIMER NODO
+	{
+		nuevo->sig=pos;
+		*raiz=nuevo;
+	}
+
+	while(ant->sig!=pos && ant->sig!=NULL)
+	ant=ant->sig;
+
+	if(NULL==ant->sig)
+	return -1;
+	nuevo->sig=ant->sig;
+	ant->sig=nuevo;
+	return 0;
 }
 
 
@@ -72,6 +120,22 @@ insertarAntesDeNodo(ListaEnlazadaRef raiz, tipoNodoRef pos, tipoInfoRef info)
 int
 insertarNodoComienzo(ListaEnlazadaRef raiz, tipoInfoRef info)
 {
+	tipoNodoRef inicio;
+
+	if(raiz==NULL)
+	return -1;
+
+	if(NULL==(inicio=creaNodo(info)))
+	return -1;
+
+	if(*raiz==NULL)
+	{
+		raiz=inicio;
+		return 0;
+	}
+	inicio->sig=*raiz;
+	*raiz=inicio;
+	return 0;
 }
 
 /*
@@ -83,6 +147,24 @@ insertarNodoComienzo(ListaEnlazadaRef raiz, tipoInfoRef info)
 int
 insertarNodoFinal(ListaEnlazadaRef raiz, tipoInfoRef info)
 {
+	tipoNodoRef final,nuevo;
+
+	if(raiz==NULL)
+	return -1;
+
+	if(NULL==(nuevo=creaNodo(info)))
+	return -1;
+	if(*raiz==NULL)
+	{
+		*raiz=nuevo;
+		return 0;
+	}
+	final=*raiz;
+	while(final->sig!=NULL)
+	final=final->sig;
+	nuevo->sig=final->sig;
+	final->sig=nuevo;
+	return 0;
 }
 
 /*
@@ -99,7 +181,7 @@ insertarNodoFinal(ListaEnlazadaRef raiz, tipoInfoRef info)
  * ineficiencias (recorridos adicionales de la lista enlazada). Opcional 
  * escribir una versión eficiente que no implique repetir recorridos.
  */
-int
+int //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 insertarNodoPosicionIndice(ListaEnlazadaRef raiz, int indice, tipoInfoRef info)
 {
 }
@@ -114,6 +196,29 @@ insertarNodoPosicionIndice(ListaEnlazadaRef raiz, int indice, tipoInfoRef info)
 int
 eliminarNodo(ListaEnlazadaRef raiz, tipoNodoRef pos)
 {
+	tipoNodoRef ant;
+
+	if(*raiz==NULL)
+	return -1;
+	ant=*raiz;
+	if(pos==NULL)
+	return -1;
+
+	if(pos==*raiz)
+	{
+		*raiz=pos->sig;
+		free(pos);
+		return 0;
+	}
+	while(ant->sig!=NULL && ant->sig!=pos)
+	ant=ant->sig;
+
+	if(ant->sig==NULL)
+	return -1;
+
+	ant->sig=pos->sig;
+	free(pos);
+	return 0;
 }
 
 
@@ -126,6 +231,17 @@ eliminarNodo(ListaEnlazadaRef raiz, tipoNodoRef pos)
 int
 eliminarNodoComienzo(ListaEnlazadaRef raiz)
 {
+	tipoNodoRef aux, aBorrar;
+
+	if(raiz==NULL || *raiz==NULL)
+	return -1;
+
+	aBorrar=*raiz;
+
+	aux=aBorrar->sig;
+	free(aBorrar);
+	*raiz=aux;
+	return 0;
 }
 
 
@@ -138,6 +254,25 @@ eliminarNodoComienzo(ListaEnlazadaRef raiz)
 int
 eliminarNodoFinal(ListaEnlazadaRef raiz)
 {
+	tipoNodoRef aux, aBorrar;
+
+	if(*raiz==NULL || raiz==NULL)
+	return -1;
+	aBorrar=*raiz;
+	if(aBorrar->sig==NULL) //UN SOLO NODO
+	{
+		free(aBorrar);
+		*raiz=NULL;
+		return 0;
+	}
+	while(aBorrar->sig!=NULL)
+	{
+		aux=aBorrar;
+		aBorrar=aBorrar->sig;
+	}
+	free(aBorrar);
+	aux->sig=NULL;
+	return 0;
 }
 
 /*
@@ -153,7 +288,7 @@ eliminarNodoFinal(ListaEnlazadaRef raiz)
  * implique ineficiencias (recorridos adicionales de la lista enlazada). 
  * Opcional escribir una versión eficiente que no implique repetir recorridos.
  */
-int
+int  //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 eliminarNodoPosicionIndice(ListaEnlazadaRef raiz, int indice)
 {
 }
@@ -169,6 +304,20 @@ eliminarNodoPosicionIndice(ListaEnlazadaRef raiz, int indice)
 int
 liberarListaEnlazada(ListaEnlazadaRef raiz)
 {
+	tipoNodoRef aBorrar, aux;
+
+	if(*raiz==NULL)
+	return -1;
+
+	aux=*raiz;
+
+	while(aux!=NULL)
+	{
+		aBorrar=aux;
+		aux=aux->sig;
+		free(aBorrar);
+	}
+	(*raiz)=NULL;
 }
 
 
@@ -201,7 +350,7 @@ liberarListaEnlazada(ListaEnlazadaRef raiz)
  * tipoInfo es un int es sencillo, si es un registro también, pero ya es
  * diferente implementación.
  */
-tipoInfo
+tipoInfo //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 devolverInfoPosicionIndice(ListaEnlazada raiz, int indice)
 {
 }
@@ -213,7 +362,7 @@ devolverInfoPosicionIndice(ListaEnlazada raiz, int indice)
  * Todos los aspectos sobre funcionamiento y valor a devolver
  * están descritos en las diapositivas
  */
-int
+int //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 insertarOrdenada(ListaEnlazadaRef raiz, tipoInfoRef info)
 {
 }
@@ -260,10 +409,162 @@ crearListaValoresAleatorios(ListaEnlazadaRef raiz, int numNodos)
 
 
 
-//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //EJ PRACT 13_________________________________________________________________________________
 
 
@@ -492,3 +793,211 @@ else { //el nodo se inserta en medio de la lista
 
 
 */
+
+
+//-------------------------------------------------------------------------------------
+// EJEMPLOS DE LISTAS ENLAZADAS
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct _agenda {
+	char nombre[20];
+	char telefono[12];
+	struct _agenda *siguiente;
+};
+
+ 
+void mostrar_menu()
+ {
+	fflush(stdout);
+	printf("\n\nMenu:\n=====\n\n");
+	printf("1.- ANadir elementos\n");
+	printf("2.- Borrar elementos\n");
+	printf("3.- Mostrar lista\n");
+	printf("4.- Salir\n\n");
+	printf("Escoge una opcion: ");
+}
+/* Con esta funcion se anade un elemento al final de la lista */
+void anadir_elemento(struct _agenda **primero)
+ {
+	struct _agenda *nuevo, *ant, *aux=*primero;
+	/* Se reserva memoria para el nuevo elemento */
+	nuevo = (struct _agenda *) malloc (sizeof(struct _agenda));
+	if (nuevo==NULL)
+	{
+		printf( "No hay memoria disponible!\n");
+		exit(0);
+
+	}
+	
+ 	printf("\nNombre a incluir: ");
+	scanf("%s%*c",nuevo->nombre);
+	printf("\nTelefono: ");
+	scanf("%s",nuevo->telefono);
+	
+       /* Ahora se introduce el nuevo elemento en la lista.
+	Se situa al final de la lista */
+	if (*primero==NULL) {
+		printf( "Primer elemento\n");
+		*primero = nuevo;
+        nuevo->siguiente=NULL;
+	}
+      else
+        { 
+         if ((strcmp(nuevo->nombre, aux->nombre))<0) 
+          {
+            nuevo->siguiente=*primero;
+            *primero=nuevo;
+          }
+         else
+          {
+           ant=NULL;
+           while(aux!=NULL && strcmp(nuevo->nombre, aux->nombre)>0)  
+           {
+             ant=aux;
+             aux=aux->siguiente;
+           }
+           if (aux==NULL)
+           {
+              ant->siguiente=nuevo;
+              nuevo->siguiente=NULL;
+          }
+          else
+          {
+              nuevo->siguiente=aux;
+              ant->siguiente=nuevo;
+          }
+         }
+    }
+}
+
+
+
+void eliminar_lista(struct _agenda **primero, char *nombre) 
+{
+
+     struct _agenda *auxiliar, *ant=NULL; /* Para recorrer la lista */
+	 int lon=20;	 
+	 nombre=(char *)malloc(lon *sizeof(char));
+ 	 printf("\nNombre a eliminar: ");
+	 scanf("%s",nombre);
+	 getchar();
+ 	for (auxiliar=*primero;auxiliar!=NULL && (strcmp(auxiliar->nombre, nombre)!=0);ant= auxiliar,auxiliar = auxiliar->siguiente);
+	{
+		if(!strcmp(auxiliar->nombre, nombre)==0)
+	    {
+         printf("\nNo se encontro ");
+         }
+         else
+         {
+             printf("\n Encontro el nombre");
+             if(ant==NULL)
+             { 
+		       if (auxiliar->siguiente == NULL)
+		       {
+                *primero=NULL; /*es el primero y el unico*/
+                free(ant);
+                }   
+	           else
+		       {
+		        auxiliar = auxiliar->siguiente;/*es el primero*/
+                *primero=auxiliar;
+                free(ant);
+                }
+             }   
+	    	else
+		    {
+		      if (auxiliar->siguiente == NULL)
+		      {
+			  /*Es el ultimo*/
+			  ant->siguiente=NULL;
+			  free(auxiliar);
+			 }
+	         else 
+		    {
+		     ant->siguiente=auxiliar->siguiente;
+		     free(auxiliar);
+		     		 }
+     }
+	}
+  }
+
+}
+
+ 
+void mostrar_lista(struct _agenda *primero) 
+{
+	struct _agenda *auxiliar; /* Para recorrer la lista */
+	int i;
+	i=0;
+	auxiliar = primero;
+ 	printf("\nMostrando la lista completa:\n");
+	while (auxiliar!=NULL)
+	{
+		printf("Nombre: %s, Telefono: %s\n",
+		auxiliar->nombre, auxiliar->telefono);
+		auxiliar = auxiliar->siguiente;
+		i++;
+	}
+if (i==0) printf( "\nLa lista esta  vacia!!\n" );
+}
+int main()
+{
+ 
+  struct _agenda *primero = NULL;
+  struct _agenda *ultimo = NULL;
+
+  char opcion;
+  char *nombre="";
+  do 
+  {
+    fflush(stdout);
+    mostrar_menu();
+    fflush(stdin);
+    opcion = getchar();
+    switch ( opcion )
+	{
+      case '1':
+           {
+            anadir_elemento(&primero);
+	         break;
+          }
+      case '2':{
+            eliminar_lista(&primero,nombre) ;
+	        break;
+         }
+      case '3': {
+               mostrar_lista(primero);
+	           break;
+            }
+      case '4': exit( 1 );
+      default:{
+                printf( "Opcion no valida\n" );
+	            break;
+             }
+}
+} while (opcion!='4');
+}
